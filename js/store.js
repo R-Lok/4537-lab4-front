9ce72577e0bfec614f9ef898e0b528b300861e35
Ryan Lok <rlok.pc@gmail.com>
2025-02-04 21:47:20 -0800

    Partially fill requests for store page

--- js/store.js
+++ js/store.js
@@ -3,3 +3,26 @@ document.getElementById('word-label').textContent = storeWordLabel
 document.getElementById('definition-label').textContent = storeDefinitionLabel
 document.getElementById('submit').textContent = storeButton
 document.getElementById('definition').placeholder = storeDefinitionPlaceholder
+
+document.getElementById('submit').addEventListener("click", async (e) => {
+    e.preventDefault()
+    const word = document.getElementById('word').value
+    const definition = document.getElementById('definition').value
+
+    if(word === '') {
+        alert("Word cannot be empty.")
+        return
+    }
+
+    if(definition === '') {
+        alert("Definition cannot be empty.")
+        return
+    }
+
+    const response = await submitWord(word, definition)
+    //fill in the logic after
+})
+
+async function submitWord(word, def) {
+    //fill in request when ready
+}
