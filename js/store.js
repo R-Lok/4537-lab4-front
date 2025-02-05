5be7e36f9691779ced68bbe31dde263ef29b0d53
Ryan Lok <rlok.pc@gmail.com>
2025-02-04 21:16:52 -0800

    Did front end for store.html

---
+++ js/store.js
@@ -0,0 +1,5 @@
+document.getElementById('title').textContent = storePageTitle
+document.getElementById('word-label').textContent = storeWordLabel
+document.getElementById('definition-label').textContent = storeDefinitionLabel
+document.getElementById('submit').textContent = storeButton
+document.getElementById('definition').placeholder = storeDefinitionPlaceholder
---
+++ lang/en/en.js
@@ -0,0 +1,7 @@
+const searchPageTitle = "Search for a word"
+const storePageTitle = "Store a word"
+
+const storeWordLabel = "Word:"
+const storeDefinitionLabel = "Definition:"
+const storeDefinitionPlaceholder = "Type the definition here.."
+const storeButton = "Submit"
